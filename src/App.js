a9640bd4d1dab9ba69d3680404e7581f0b2e86c7
Giorgi Patsia <gio.patsia@gmail.com>
2023-05-01 22:55:52 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "./reset.css";
+import todoimg from "./images/TODO.png";
+import { useState } from "react";
+import { createElement } from "react";
 
 function App() {
+  const [isCheked, setIsCheked] = useState(true);
+  const setTheme = (theme) => (document.documentElement.className = theme);
+  function changeTheme() {
+    setIsCheked(!isCheked);
+    if (isCheked) {
+      console.log(isCheked);
+      setTheme("light");
+    } else {
+      setTheme("dark");
+      console.log(isCheked);
+    }
+  }
+
+  function handleCheckbox() {
+    console.log(isCheked);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className="header">
+        <img src={todoimg} alt="Todo Logo" />
+        <input type="checkbox" id="themeChanger" onChange={changeTheme} />
+      </div>
+      <form className="formDiv">
+        <input
+          type="checkbox"
+          id="todoCheckbox"
+          onClick={(handleCheckbox, createElement)}
+        />
+        <input
+          type="text"
+          placeholder="Create a new todo..."
+          id="todoInput"
+          required
+        />
+      </form>
     </div>
   );
 }
